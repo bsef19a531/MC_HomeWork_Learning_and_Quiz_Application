c5222951a0767d71a33f83c7ded590aeaf7cfb5c
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-05-22 22:05:28 +0500

    Added colors values

---
+++ app/src/main/java/com/example/phplearningapp/Introduction_PHP.java
@@ -0,0 +1,14 @@
+package com.example.phplearningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Introduction_PHP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction_php);
+    }
+}
