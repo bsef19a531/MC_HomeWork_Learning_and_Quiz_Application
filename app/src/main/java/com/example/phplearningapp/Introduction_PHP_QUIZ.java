b5c8a0f20d9362d69c3dabfdce7d04ecde24afe4
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-05-23 01:11:45 +0500

    Added Quiz logic Functions

--- app/src/main/java/com/example/phplearningapp/Introduction_PHP_QUIZ.java
+++ app/src/main/java/com/example/phplearningapp/Introduction_PHP_QUIZ.java
@@ -2,7 +2,10 @@ package com.example.phplearningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class Introduction_PHP_QUIZ extends AppCompatActivity {
 
@@ -12,5 +15,42 @@ public class Introduction_PHP_QUIZ extends AppCompatActivity {
         setContentView(R.layout.activity_introduction_php_quiz);
     }
 
+    public void submitQuiz(View view)
+    {
+        RadioButton q1_1 = findViewById(R.id.Q1_1);
+        RadioButton q1_2 = findViewById(R.id.Q1_2);
+        RadioButton q1_3 = findViewById(R.id.Q1_3);
 
+        RadioButton q2_1 = findViewById(R.id.Q2_1);
+        RadioButton q2_2 = findViewById(R.id.Q2_2);
+        RadioButton q2_3 = findViewById(R.id.Q2_3);
+
+        RadioButton q3_1 = findViewById(R.id.Q3_1);
+        RadioButton q3_2 = findViewById(R.id.Q3_2);
+        RadioButton q3_3 = findViewById(R.id.Q3_3);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Introduction_PHP_QUIZ.this);
+        if(q1_2.isChecked() && q2_1.isChecked() && q3_1.isChecked())
+        {
+            builder.setMessage("Q1, Q2 and Q3 are Correct!");
+        }
+        else if (q1_2.isChecked() && q2_1.isChecked())
+        {
+            builder.setMessage("Q1 and Q2 are Correct!");
+        }
+        else if(q1_2.isChecked() && q3_1.isChecked())
+        {
+            builder.setMessage("Q1 and Q3 are Correct!");
+        }
+        else if(q2_1.isChecked() && q3_1.isChecked())
+        {
+            builder.setMessage("Q2 and Q3 are Correct!");
+        }
+        else
+        {
+            builder.setMessage("Sorry Try Again!");
+        }
+        builder.setTitle("Result");
+        builder.setCancelable(true);
+    }
 }
